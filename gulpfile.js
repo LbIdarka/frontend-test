e0782c806a1beac25d9d882044664723bd2557ed
Alena Nikolaenko <lbidarka@gmail.com>
2017-06-15 14:06:53 +0300

    сборка

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,11 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var precss = require("precss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+var del = require("del");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -17,13 +22,19 @@ gulp.task("style", function() {
         "last 19 Chrome versions",
         "last 13 Firefox versions",
         "last 4 Safari versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
     server: ".",
     notify: false,
@@ -34,4 +45,24 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("postcss/**/*.css", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", fn);
 });
